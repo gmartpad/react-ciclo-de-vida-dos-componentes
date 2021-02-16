2301770f48a0939a3dcc235cda549462356be7b5
gmartpad <gabrielmpwork@gmail.com>
2021-02-16 00:45:26 -0300

    adicionando categorias aos cards

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -9,6 +9,7 @@ class CardNota extends Component {
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
           {/* <img src={deleteSVG}/> */}
           <DeleteSVG onClick={() => this.props.apagarNota(this.props.index)}/>
+          <h4>{this.props.categoria}</h4>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
